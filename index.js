4043513a5f8e3bf502c7b1f924b75400ebb969b8
Ikechukwu <iykekings36@gmail.com>
2019-07-25 08:33:48 +0100

    config eslint

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const server = express()
+const server = express();
 
 server.use(express.json());
 
 server.get('/', (req, res) => {
-  res.status(200).json('Server live at /api/users')
-})
+  res.status(200).json('Server live at /api/users');
+});
 
 server.listen(4000, () => {
-  console.log('Server running at port 4000')
-})
+  console.log('Server running at port 4000');
+});
