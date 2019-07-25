c83f548f1c74c0c35f46259f5b9946fa855c5b7c
Ikechukwu <iykekings36@gmail.com>
2019-07-25 08:32:24 +0100

    create server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const server = express()
+
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.status(200).json('Server live at /api/users')
+})
+
+server.listen(4000, () => {
+  console.log('Server running at port 4000')
+})
